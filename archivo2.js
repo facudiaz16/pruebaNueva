dab3ddeb8a8e22839fd66aa4220ad6e39c11c94d
Martin Zalazar <martinzetav@gmail.com>
2024-05-23 18:54:36 -0300

    Se agrego linea 7 al archivo2.js

--- archivo2.js
+++ archivo2.js
@@ -4,7 +4,7 @@ console.log("3 Linea de participante B")
 console.log("4 linea del participante A");
 console.log("5 Linea de participante B")
 console.log("6 linea del participante A");
-
+console.log("Line< 7 agregada por el participante B");
 
 
 
