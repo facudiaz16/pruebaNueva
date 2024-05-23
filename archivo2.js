642efd1efc3befb41240d09e54cf8bc780a26ea0
facudiaz16 <fdiazbordon@gmail.com>
2024-05-23 18:45:51 -0300

    cambie A por B

--- archivo2.js
+++ archivo2.js
@@ -1,9 +1,9 @@
 console.log("1 Linea de participante B")
-console.log("2 linea del participante A");
+console.log("2 linea del participante B");
 console.log("3 Linea de participante B")
-console.log("4 linea del participante A");
+console.log("4 linea del participante B");
 console.log("5 Linea de participante B")
-console.log("6 linea del participante A");
+console.log("6 linea del participante B");
 
 
 
